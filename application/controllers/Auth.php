eb3ae58c0e4d866a2e9b155e64cb1c1928d7e848
Ivettita <ivettita_96@live.com.mx>
2020-11-17 11:24:33 -0600

    subir archivos

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -1,6 +1,9 @@
 <?php
 
+//defined('BASEPATH') OR exit('No direct script access allowed');
+
 require_once APPPATH . '/libraries/REST_Controller.php';
+require APPPATH . 'libraries/REST_Controller_Definitions.php';
 
 class Auth extends REST_Controller {
 
@@ -46,4 +49,79 @@ class Auth extends REST_Controller {
      
         $this->response(['Usuario creado'], REST_Controller::HTTP_OK);
     } 
+
+       
+       function upload_post() {
+       // Acceso a las cabeceras
+           header("Access-Control-Allow-Origin: *");
+       //   $_POST = json_decode($this->security->xss_clean(file_get_contents("php://input")),true);
+           header('Access-Control-Allow-Methods: POST');
+           header('Access-Control-Allow-Headers: Content-Type');
+            
+        // Condicion para subir archivo
+        if ($this->input->method()) {
+
+            if(!$_FILES) {
+                $this->response('Por favor escoje un archivo', 500);
+                return;
+            }
+            
+            //ruta de la carpeta en donde se van a suibr los archivos
+            $upload_path = '/uploads/';
+            //ruta destino
+            $config['upload_path'] = $upload_path;
+            //se permite todo tipo de archivo (imagen y audios)
+            $config['allowed_types'] = '*';
+            //permitir el peso maximo del archivo (0 significa que no tiene limite de peso) 
+            $config['max_size'] = '0';
+            //caracteres (largo de nombre)
+            $config['max_filename'] = '255';
+            //encriptar el nombre del archivo
+            //$config['encrypt_name'] = TRUE;
+            
+            $this->load->library('upload', $config);
+            
+            //si la carpeta exite nos mandara a decir que ya se encuentra.
+            //en caso de que no exista la crea
+            if (file_exists($upload_path . $_FILES['file']['name'])) {
+                $this->response('El archivo ya existe => ' . $upload_path . $_FILES['file']['name']);
+                return;
+            } else {
+                if (!file_exists($upload_path)) {
+                    mkdir($upload_path, 0777, true);
+                }
+                
+                //subir el archivo y capturar el post
+                if($this->upload->do_upload('file')) {
+                                    //
+                                         $problema = $this->input->post('problema');
+                                         $imagen = 'http://' . $_SERVER['SERVER_NAME'] . $upload_path . $_FILES['file']['name'];
+                                         $audio = 'http://' . $_SERVER['SERVER_NAME'] . $upload_path . $_FILES['file']['name'];
+                                         $ejercicio_id = $this->input->post('ejercicio_id');
+
+
+                                         
+                                         $data = array (
+                                                'problema'=>$problema,
+                                                'imagen'=>$imagen,
+                                                'audio'=>$audio,
+                                                'ejercicio_id'=>$ejercicio_id
+                                            );
+                                         //insertar datos en la base
+                                       $this->db->insert('ejercicio_contenidos',$data);
+
+                                       $response=$this->response('si se pudo');
+
+                    return $response;
+
+                } else {
+                        //en caso de que no te manda el mensaje de error
+                    $this->response('Error al subir el archivo =>' . $this->upload->display_errors(), 500);
+                    return;
+                }
+            }
+   
+        }
+    }
+
 }
